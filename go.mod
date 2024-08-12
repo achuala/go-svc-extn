b6a833527713b16c7476e52d3c1e5a15eb174d14
KR <kartraman@gmail.com>
2024-08-12 22:49:33 +0530

    Dep update

--- go.mod
+++ go.mod
@@ -4,47 +4,48 @@ go 1.22.5
 
 require (
 	github.com/btcsuite/btcutil v1.0.2
+	github.com/bufbuild/protovalidate-go v0.6.3
+	github.com/dchest/siphash v1.2.3
 	github.com/dgraph-io/ristretto v0.1.1
+	github.com/go-crypt/crypt v0.2.25
 	github.com/go-kratos/kratos/v2 v2.8.0
 	github.com/godruoyi/go-snowflake v0.0.2
 	github.com/google/uuid v1.6.0
+	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf
 	github.com/lithammer/shortuuid/v4 v4.0.0
+	github.com/pkg/errors v0.9.1
 	github.com/redis/go-redis/v9 v9.6.1
 	github.com/santhosh-tekuri/jsonschema/v5 v5.3.1
 	github.com/stretchr/testify v1.9.0
+	github.com/tink-crypto/tink-go/v2 v2.2.0
+	golang.org/x/crypto v0.26.0
 	google.golang.org/protobuf v1.34.2
+	gorm.io/driver/postgres v1.5.9
+	gorm.io/gorm v1.25.11
 )
 
 require (
-	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240508200655-46a4cf4ba109.2 // indirect
-	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
-	github.com/bufbuild/protovalidate-go v0.6.3 // indirect
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.34.2-20240717164558-a6c49f84cc0f.2 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.1 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/dchest/siphash v1.2.3 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/dustin/go-humanize v1.0.0 // indirect
-	github.com/go-crypt/crypt v0.2.25 // indirect
-	github.com/go-crypt/x v0.2.18 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-crypt/x v0.2.19 // indirect
 	github.com/go-kratos/aegis v0.2.0 // indirect
 	github.com/go-playground/form/v4 v4.2.1 // indirect
-	github.com/golang/glog v1.2.1 // indirect
-	github.com/google/cel-go v0.20.1 // indirect
+	github.com/golang/glog v1.2.2 // indirect
+	github.com/google/cel-go v0.21.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
-	github.com/inhies/go-bytesize v0.0.0-20220417184213-4913239db9cf // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.6.0 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/kr/text v0.2.0 // indirect
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stoewer/go-strcase v1.3.0 // indirect
-	github.com/tink-crypto/tink-go/v2 v2.2.0 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/exp v0.0.0-20240325151524-a685a6edb6d8 // indirect
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa // indirect
 	golang.org/x/net v0.28.0 // indirect
 	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
@@ -53,6 +54,4 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240812133136-8ffd90a71988 // indirect
 	google.golang.org/grpc v1.65.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.5.9 // indirect
-	gorm.io/gorm v1.25.11 // indirect
 )
---
+++ pkg/serverextn/middleware/logging.go
@@ -0,0 +1,176 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/achuala/go-svc-extn/gen/go/options"
+	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
+	"github.com/go-kratos/kratos/v2/transport"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/types/descriptorpb"
+)
+
+type Redacter interface {
+	Redact() string
+}
+
+// Server is an server logging middleware.
+func Server(logger log.Logger) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+			startTime := time.Now()
+			if info, ok := transport.FromServerContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+			}
+			reply, err = handler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			_ = log.WithContext(ctx, logger).Log(level,
+				"kind", "server",
+				"component", kind,
+				"op", operation,
+				"req", extractArgs(req),
+				"resp", extractArgs(reply),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+		}
+	}
+}
+
+// Client is a client logging middleware.
+func Client(logger log.Logger) middleware.Middleware {
+	return func(handler middleware.Handler) middleware.Handler {
+		return func(ctx context.Context, req interface{}) (reply interface{}, err error) {
+			var (
+				code      int32
+				reason    string
+				kind      string
+				operation string
+			)
+			startTime := time.Now()
+			if info, ok := transport.FromClientContext(ctx); ok {
+				kind = info.Kind().String()
+				operation = info.Operation()
+			}
+			reply, err = handler(ctx, req)
+			if se := errors.FromError(err); se != nil {
+				code = se.Code
+				reason = se.Reason
+			}
+			level, stack := extractError(err)
+			_ = log.WithContext(ctx, logger).Log(level,
+				"kind", "client",
+				"component", kind,
+				"op", operation,
+				"req", extractArgs(req),
+				"resp", extractArgs(reply),
+				"code", code,
+				"reason", reason,
+				"stack", stack,
+				"latency", time.Since(startTime).Seconds(),
+			)
+			return
+		}
+	}
+}
+
+// extractArgs returns the string of the req
+func extractArgs(req interface{}) string {
+	if protoMsg, ok := req.(proto.Message); ok {
+		clone := proto.Clone(protoMsg)
+		handleSenstiveData(clone.ProtoReflect())
+		return fmt.Sprintf("%+v", clone)
+	} else if redacter, ok := req.(Redacter); ok {
+		return redacter.Redact()
+	} else if stringer, ok := req.(fmt.Stringer); ok {
+		return stringer.String()
+	}
+	return fmt.Sprintf("%+v", req)
+}
+
+// extractError returns the string of the error
+func extractError(err error) (log.Level, string) {
+	if err != nil {
+		return log.LevelError, fmt.Sprintf("%+v", err)
+	}
+	return log.LevelInfo, ""
+}
+
+func handleSenstiveData(m protoreflect.Message) {
+	m.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		opts := fd.Options().(*descriptorpb.FieldOptions)
+
+		switch typed := v.Interface().(type) {
+		case protoreflect.Message:
+			handleSenstiveData(typed)
+		case protoreflect.Map:
+			typed.Range(func(key protoreflect.MapKey, value protoreflect.Value) bool {
+				if _, ok := value.Interface().(protoreflect.Message); ok {
+					handleSenstiveData(value.Message())
+				}
+				if _, ok := key.Interface().(protoreflect.Message); ok {
+					handleSenstiveData(key.Value().Message())
+				}
+				return true
+			})
+		case protoreflect.List:
+			for i := 0; i < typed.Len(); i++ {
+				if _, ok := typed.Get(i).Interface().(protoreflect.Message); ok {
+					handleSenstiveData(typed.Get(i).Message())
+				}
+			}
+		}
+
+		// Get extension from field
+		ext := proto.GetExtension(opts, options.E_Sensitive)
+		// Check if equal to bool as expected
+		extVal, ok := ext.(*options.Sensitive)
+		if !ok {
+			return true
+		}
+
+		// If true clear field and move on
+		if extVal != nil {
+			if extVal.GetRedact() || extVal.Pii {
+				m.Clear(fd)
+			} else if extVal.GetMask() {
+				maskedValue := maskString(v.String())
+				m.Set(fd, protoreflect.ValueOfString(maskedValue))
+			}
+		}
+
+		return true
+	})
+
+}
+
+func maskString(value string) string {
+	if len(value) <= 4 {
+		// If the string length is less than or equal to 4, just return "****" for masking.
+		return "****"
+	}
+
+	// Mask all characters except the last 4 with "*".
+	maskedValue := strings.Repeat("*", len(value)-4) + value[len(value)-4:]
+	return maskedValue
+}
