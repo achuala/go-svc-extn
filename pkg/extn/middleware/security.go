f012112874b7c8649011e3e94f3ebd2a18478ca4
Karthik Raman <kr@karplexus.com>
2025-04-05 21:26:32 +0530

    Added support for reading rid from metadata

--- pkg/extn/middleware/security.go
+++ pkg/extn/middleware/security.go
@@ -5,9 +5,9 @@ import (
 
 	"github.com/achuala/go-svc-extn/pkg/util/idgen"
 	"github.com/go-kratos/kratos/v2/errors"
+	"github.com/go-kratos/kratos/v2/metadata"
 	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/transport"
-	"google.golang.org/grpc/metadata"
 )
 
 // CtxKey is a custom type for context keys
@@ -19,8 +19,9 @@ const (
 	CtxSystemPeerKey      CtxKey = "x-system-peer"
 	CtxSignedHeadersKey   CtxKey = "x-signed-headers"
 	CtxAuthorizationKey   CtxKey = "Authorization"
-	CtxRequestIDKey       CtxKey = "x-request-id"
-	CtxMdCorrelationIdKey CtxKey = "x-md-correlation-id"
+	CtxRequestIDKey       CtxKey = "rid"
+	CtxMdCorrelationIdKey CtxKey = "x-md-global-correlation-id"
+	CtxMdRequestIdKey     CtxKey = "x-md-global-request-id"
 )
 
 // getCorrelationIdFromCtx retrieves the correlation ID from the context or generates a new one
@@ -33,13 +34,15 @@ func getCorrelationIdFromCtx(ctx context.Context) string {
 	return idgen.NewId()
 }
 
-func getCorrelationIdFromMetadata(md metadata.MD) string {
-	if values := md.Get(string(CtxCorrelationIdKey)); len(values) > 0 {
-		return values[0]
-	} else if values := md.Get(string(CtxRequestIDKey)); len(values) > 0 {
-		return values[0]
+func getCorrelationIdFromMetadata(ctx context.Context) string {
+	if md, ok := metadata.FromServerContext(ctx); ok {
+		if values := md.Get(string(CtxMdCorrelationIdKey)); len(values) > 0 {
+			return string(values[0])
+		} else if values := md.Get(string(CtxMdRequestIdKey)); len(values) > 0 {
+			return string(values[0])
+		}
 	}
-	return ""
+	return idgen.NewId()
 }
 
 // ServerSecurityHeaderValidator middleware validates the presence of required security headers
@@ -65,7 +68,10 @@ func ServerCorrelationIdInjector() middleware.Middleware {
 			if tr, ok := transport.FromServerContext(ctx); ok {
 				correlationId := tr.RequestHeader().Get(string(CtxCorrelationIdKey))
 				if correlationId == "" {
-					correlationId = idgen.NewId()
+					correlationId = tr.RequestHeader().Get(string(CtxMdCorrelationIdKey))
+					if correlationId == "" {
+						correlationId = idgen.NewId()
+					}
 				}
 				ctx = context.WithValue(ctx, CtxCorrelationIdKey, correlationId)
 				ctx = transport.NewServerContext(ctx, tr)
@@ -83,6 +89,7 @@ func ClientCorrelationIdInjector() middleware.Middleware {
 				correlationId := getCorrelationIdFromCtx(ctx)
 				tr.RequestHeader().Set(string(CtxCorrelationIdKey), correlationId)
 				tr.RequestHeader().Set(string(CtxMdCorrelationIdKey), correlationId)
+				ctx = metadata.AppendToClientContext(ctx, string(CtxMdCorrelationIdKey), correlationId)
 			}
 			return handler(ctx, req)
 		}
