841d18759b04f7e69f3062551f5016d06f0f6459
Karthik Raman <kr@karplexus.com>
2025-04-07 11:12:40 +0530

    updated with new id generation

--- pkg/util/idgen/id_generator.go
+++ pkg/util/idgen/id_generator.go
@@ -103,7 +103,11 @@ func NewIdWithPrefix(prefix string) string {
 }
 
 func NewUUID() string {
-	return uuid.NewString()
+	id, err := uuid.NewV7()
+	if err != nil {
+		return ""
+	}
+	return id.String()
 }
 
 // Generates a new ID, based on snowflake implementation.
